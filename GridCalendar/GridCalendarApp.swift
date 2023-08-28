46dceada52f5a6b5c09aa0275ffd6a08da606ae2
User <user@MB033361.local>
2023-08-28 11:32:24 -0300

    initial commit

---
+++ GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by User on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
